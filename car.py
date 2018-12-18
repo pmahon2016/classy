cf7c1e24e6ff995fbc1ad5a10f734b2797b1c913
paulmahon@me.com <g9QD3Z66ij9w>
2018-12-18 10:33:28 -0500

    Part II of Classes

--- car.py
+++ car.py
@@ -1,9 +1,38 @@
-class Car:
+class Cars:
+    """Simple Class with Template for Cars"""
 
-    def __init__(self, xloc, yloc,image):
+    dealer = "Bestcars"
 
-        self.xloc = xloc
-        self.yloc = yloc
-        self.image = image
+    def __init__(self, type, color):
+        self.type = type
+        self.color = color
+
+    def describe_car(self):
+        print("My car is a " + self.color + " " + self.type)
+        print("this is from " + Cars.dealer)
+
+class Electric_cars(Cars):
+
+    def __init__(self, type, color, capacity):
+        super().__init__(type, color)
+
+        self.capacity = capacity
+
+    def describe_car(self):
+        print("My car is a " + self.color + " " + self.type
+              + " that has a range of " + self.capacity)
+        print("this is from " + self.dealer)
+
+# instances of Car class
+car1 = Cars('sedan', 'blue')
+car2 = Cars('SUV', 'green')
+
+ecars = Electric_cars('sedan', 'green', '180miles')
 
 
+# call method within Car Class
+car1.describe_car()
+
+car2.describe_car()
+
+ecars.describe_car()
--- class_test.py
+++ class_test.py
@@ -1,8 +1,39 @@
-class Dog:
+class Cars:
+    """Simple Class with Template for Cars"""
 
-    def __init__(self, name, age):
-        self.name = 'shep'
-        self.age = age
+    dealer = 'Best Cars'
 
-    def rollover(self):
-        print(self.name + " is a big dog")
+    def __init__(self, type, color):
+        self.type = type
+        self.color = color
+
+    def describe_car(self):
+        print("My car is a " + self.color + " " + self.type)
+        print("This car was purchased at " + Cars.dealer)
+
+
+class Electric_cars(Cars):
+    def __init__(self, type, color, capacity):
+        super().__init__(type, color)
+
+        self.capacity = capacity
+
+    def describe_car(self):
+        print("My car is a " + self.color + " " + self.type
+              + " that has a range of " + self.capacity)
+        print("This car was purchased at " + Cars.dealer)
+
+
+# instances of Car class
+car1 = Cars('sedan', 'blue')
+car2 = Cars('SUV', 'green')
+ecar = Electric_cars('Sedan', 'Green', '160miles')
+
+# call method within Car Class
+car1.describe_car()
+
+car2.describe_car()
+
+ecar.describe_car()
+
+Electric_cars.describe_car(ecar)
